0e16dd69864304e7346aaa757a201b408924f414
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-07 15:29:22 +0200

    Unify atom modules and flatten source layout

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
         .visionOS(.v27),
     ],
     products: [
-        .library(
-            name: "Either",
-            targets: ["Either"]
-        )
+        .library(name: "Either", targets: ["Either"]),
+        .library(name: "Either Standard Library Integration", targets: ["Either Standard Library Integration"]),
+        .library(name: "Either Foundation Library Integration", targets: ["Either Foundation Library Integration"]),
+        .library(name: "Either Test Support", targets: ["Either Test Support"]),
     ],
     dependencies: [
         .package(
@@ -35,27 +35,51 @@ let package = Package(
         .target(
             name: "Either",
             dependencies: [
-                .product(name: "Equation Protocol", package: "swift-equation"),
-                .product(name: "Hash Protocol", package: "swift-hash"),
-                .product(name: "Comparison Protocol", package: "swift-comparison"),
-            ]
+                .product(name: "Equation", package: "swift-equation"),
+                .product(name: "Hash", package: "swift-hash"),
+                .product(name: "Comparison", package: "swift-comparison"),
+            ],
+            path: "Sources/Either"
+        ),
+        .target(
+            name: "Either Standard Library Integration",
+            dependencies: [
+                .target(name: "Either"),
+            ],
+            path: "Sources/Either Standard Library Integration"
+        ),
+        .target(
+            name: "Either Foundation Library Integration",
+            dependencies: [
+                .target(name: "Either"),
+                .target(name: "Either Standard Library Integration"),
+            ],
+            path: "Sources/Either Foundation Library Integration"
+        ),
+        .target(
+            name: "Either Test Support",
+            dependencies: [
+                .target(name: "Either"),
+            ],
+            path: "Tests/Support"
         ),
         .testTarget(
             name: "Either Tests",
             dependencies: [
                 .target(name: "Either"),
-                .product(
-                    name: "Hash Standard Library Integration",
-                    package: "swift-hash"
-                ),
-            ]
+                .product(name: "Hash Standard Library Integration", package: "swift-hash"),
+                .target(name: "Either Test Support"),
+                .target(name: "Either Standard Library Integration"),
+                .target(name: "Either Foundation Library Integration"),
+            ],
+            path: "Tests/Either Tests"
         ),
     ],
     swiftLanguageModes: [.v6]
 )
 
-for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
-    let ecosystem: [SwiftSetting] = [
+for target in package.targets {
+    target.swiftSettings = [
         .strictMemorySafety(),
         .enableUpcomingFeature("ExistentialAny"),
         .enableUpcomingFeature("InternalImportsByDefault"),
@@ -64,8 +88,4 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableExperimentalFeature("Lifetimes"),
         .enableUpcomingFeature("InferIsolatedConformances"),
     ]
-
-    let package: [SwiftSetting] = []
-
-    target.swiftSettings = (target.swiftSettings ?? []) + ecosystem + package
 }
---
+++ Sources/Either Foundation Library Integration/exports.swift
@@ -0,0 +1,3 @@
+@_exported public import Either
+@_exported public import Either_Standard_Library_Integration
+@_exported public import Foundation
